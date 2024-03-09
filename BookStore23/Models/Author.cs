5209c1b0beb3cb8b67cb52b0b3ad6f90ee0ac4cf
User <User@DESKTOP-7K0JUB1>
2024-03-09 16:41:45 +0300

    Add project files.

---
+++ BookStore23/Models/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore23.Models
+{
+    public class Author
+    {
+        public int id { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ BookStore23/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore23.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Author Author { get; set; }
+
+    }
+}
