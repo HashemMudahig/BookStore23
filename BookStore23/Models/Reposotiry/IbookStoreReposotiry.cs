21180231bd371d6c953010acde13fc5cf875a0f8
User <User@DESKTOP-7K0JUB1>
2024-03-11 23:34:22 +0300

    Make the Implemention of BookReposotiry

--- BookStore23/Models/Reposotiry/IbookStoreReposotiry.cs
+++ BookStore23/Models/Reposotiry/IbookStoreReposotiry.cs
@@ -5,7 +5,7 @@
         IList<TEntity> List();
         TEntity find(int id);
         void Add(TEntity entity);
-        void Update(TEntity entity);
+        void Update(int id ,TEntity entity);
         void Delete(int id);    
     }
 }
