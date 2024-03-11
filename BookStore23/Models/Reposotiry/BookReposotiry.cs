d4f3235302e24812c20f2c9cf6f935c41e252d75
User <User@DESKTOP-7K0JUB1>
2024-03-11 22:54:39 +0300

    Create class and interface in Models folser

---
+++ BookStore23/Models/Reposotiry/BookReposotiry.cs
@@ -0,0 +1,31 @@
+﻿
+namespace BookStore23.Models.Reposotiry
+{
+    public class BookReposotiry : IbookStoreReposotiry<Book>//':' this is for implement 
+    {
+        public void Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
