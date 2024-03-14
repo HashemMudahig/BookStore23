1b81d48e45c93c2c15ce559a31538f2838eb7324
User <User@DESKTOP-7K0JUB1>
2024-03-14 23:09:57 +0300

    Add controller and View

---
+++ BookStore23/Controllers/AuthorController.cs
@@ -0,0 +1,93 @@
+﻿using BookStore23.Models;
+using BookStore23.Models.Reposotiry;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore23.Controllers
+{
+    
+    public class AuthorController : Controller
+    {
+        private readonly IbookStoreReposotiry<Author> authorRepository;
+
+        public AuthorController(IbookStoreReposotiry<Author> authorRepository)//we make injection of type Author
+        {
+            this.authorRepository = authorRepository;
+        }
+        // GET: AuthorController
+        public ActionResult Index()
+        {
+            var authors = authorRepository.List();
+            return View(authors);
+        }
+
+        // GET: AuthorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AuthorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AuthorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AuthorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
