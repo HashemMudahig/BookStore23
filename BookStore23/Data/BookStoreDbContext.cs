8a621c7f95795e2473183900b718f70f687e1f36
User <User@DESKTOP-7K0JUB1>
2024-03-28 22:16:11 +0300

    Create a DataBase

---
+++ BookStore23/Data/BookStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BookStore23.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookStore23.Data
+{
+    public class BookStoreDbContext:DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options) // then define all these 
+        {
+
+        }
+        public DbSet<Author> Authors { get; set; }// these are the the tables in the database will named like this 
+        public DbSet<Book> Books { get; set; }
+        public DbSet<user> Users { get; set; }// using CTL+. to get the red line out 
+    }
+}
